c3176daed85f89a9e29095077f32c218ffe4d4c4
gamalielara <ara.gamaliel@ui.ac.id>
2021-07-26 19:52:15 +0700

    first commit

---
+++ opening.js
@@ -0,0 +1,5 @@
+// tl to change together multiple animations
+const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
+tl.to('.text', {y:'0%', opacity: '1', duration: 1, stagger: 1});
+tl.to('.intro', {y: '-100%', duration: 2, delay: .5});
+tl.to('.slider', {y: '-100%', duration: 1.5}, '-=1.75');
---
+++ script.js
@@ -0,0 +1,62 @@
+// javascript for nav bar effects on scroll
+window.addEventListener('scroll', function(){
+    const header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+});
+
+// javascript of responsive navigation sidebar menu
+const menuBtn = document.querySelector('.menu-btn');
+const navigation = document.querySelector('.navigation');
+const navigationItems = document.querySelectorAll('.navigation a');
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('active');
+    navigation.classList.toggle('active');
+});
+
+navigationItems.forEach((navigationItem) => {
+    navigationItem.addEventListener('click', () => {
+        menuBtn.classList.remove('active');
+        navigation.classList.remove('active');
+    });
+});
+
+// to make the caption img hidden
+const imgDiv = document.querySelectorAll('.work > .content > .card > .card-img');
+document.querySelectorAll('.img-caption').forEach((caption) => {caption.classList.add('hidden')});
+document.querySelectorAll('.card-fullpage').forEach((fullPreview) => {fullPreview.style.display = 'none';});
+document.querySelectorAll('.container-fullpage').forEach((container) => {container.style.opacity = '0';});
+
+imgDiv.forEach((img) => {
+const captionDiv = img.childNodes[1];
+    img.addEventListener('mouseover', () => {
+        captionDiv.classList.remove('hidden');
+    });
+
+    img.addEventListener('click', () => {
+        captionDiv.classList.remove('hidden');
+    });
+    
+    img.addEventListener('mouseleave', () => {
+        captionDiv.classList.add('hidden');
+    });
+});
+
+const closeButton = document.querySelectorAll('#close');
+if(closeButton !== null){
+    closeButton.forEach((close) => {
+        close.addEventListener('click', (event) => {
+            const theFullPageContainer = event.target.parentElement.parentElement.parentElement;
+            document.querySelectorAll('.container-fullpage').forEach((container) => {container.style.opacity = '0';});
+            theFullPageContainer.style.display = 'none';
+        })
+    })
+}
+
+const imageCaption = document.querySelectorAll('.content .card .card-img .img-caption h5');
+imageCaption.forEach((caption) => {
+    caption.addEventListener('click', (event) => {
+        const imgfull = event.target.parentElement.parentElement.parentElement.childNodes[1];
+        imgfull.style.display = 'block';
+        document.querySelectorAll('.container-fullpage').forEach((container) => {container.style.opacity = '1';});
+    })
+});
